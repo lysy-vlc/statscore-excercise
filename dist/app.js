80efce130735cc9c5e1e9d995603b766fc71c080
lysy-vlc <kamil.grzaba@gmail.com>
2023-05-31 15:38:52 -0500

    creates event parser with is match valid and make match name methods

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const isMatchValid_validator_1 = require("./validators/isMatchValid.validator");
+const matches_db_1 = require("./db/matches.db");
+const isSportValid_validator_1 = require("./validators/isSportValid.validator");
+const getMatchTeamsSeparator_helper_1 = require("./helpers/getMatchTeamsSeparator.helper");
+class EventParser {
+    isMatchValid(match) {
+        return (0, isMatchValid_validator_1.isMatchValid)(match);
+    }
+    makeMatchName(match) {
+        const isMatchAndSportValid = (0, isMatchValid_validator_1.isMatchValid)(match) && (0, isSportValid_validator_1.isSportValid)(match.sport);
+        if (isMatchAndSportValid) {
+            return `${match.participant1} ${(0, getMatchTeamsSeparator_helper_1.getMatchTeamsSeparator)(match)} ${match.participant2}`;
+        }
+        return 'Exception: invalid sport';
+    }
+}
+const parser = new EventParser();
+matches_db_1.matches.forEach(m => {
+    console.log(parser.makeMatchName(m));
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
+import { isMatchValid, Match, ValidMatchCandidate } from './validators/isMatchValid.validator'
+import { isSportValid } from './validators/isSportValid.validator'
+import { getMatchTeamsSeparator } from './helpers/getMatchTeamsSeparator.helper'
 
+class EventParser {
+  isMatchValid(match: ValidMatchCandidate): boolean {
+    return isMatchValid(match);
+  }
+
+  makeMatchName(match: ValidMatchCandidate): string | Error {
+    const isMatchAndSportValid = isMatchValid(match) && isSportValid(match.sport);
+
+    if (isMatchAndSportValid) {
+      return `${match.participant1} ${getMatchTeamsSeparator(match as Match)} ${match.participant2}`;
+    }
+
+    return 'Exception: invalid sport';
+  }
+}
